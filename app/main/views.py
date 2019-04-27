8a095e4a830246b853ecbb4cc8034574b4ab2fe6
Aisa <aisajemila@gmail.com>
2019-04-27 18:13:40 +0300

    added two functions to get the random quote and another that proceses the data from the quote url.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from . import main
 from flask import render_template,request,redirect,url_for,abort,flash
+from ..request import get_quote
 
 @main.route('/')
 def index():
-  
+  random_quote = get_quote()  
 
-  return render_template('index.html')
+  return render_template('index.html',quote=random_quote)
--- app/request.py
+++ app/request.py
@@ -2,7 +2,7 @@ import urllib.request,json
 from .models import Quote
 
 #Getting apikey
-apiKey = None
+base_url = None
 
 def configure_request(app):
   global base_url
@@ -13,3 +13,27 @@ def get_quote():
   Function that get the json response to our url request
   '''
   get_quote_url = base_url.format
+  with urllib.request.urlopen(get_quote_url) as url:
+    get_quote_data = url.read()
+    get_quote_response = json.loads(get_quote_data)
+
+    random_quote = None
+
+    if get_quote_response:
+      new_quote =  get_quote_response
+      random_quote = process_quote(new_quote)
+
+  return random_quote
+
+def process_quote(quote):
+  '''
+  Function that proces the quote source to an object
+  '''
+  id = quote.get('id')
+  author = quote.get('author')
+  quote = quote.get('quote')
+
+  quote_object = Quote(id,author,quote)
+
+  return quote_object
+
